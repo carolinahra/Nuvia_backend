59b8e1cc238f37453800aea45259b3d250e0562a
Carolina Herrera <carolinahradiaz@gmail.com>
2026-05-26 20:26:56 +0200

    modified auth subscriber to add recover password to authorized routes without credentials

--- src/EventSubscriber/AuthSubscriber.php
+++ src/EventSubscriber/AuthSubscriber.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpKernel\KernelEvents;
 
 class AuthSubscriber implements EventSubscriberInterface
 {
-    private const PUBLIC_ROUTES = ['login', 'register', 'procesar_contacto'];
+    private const PUBLIC_ROUTES = ['login', 'register', 'procesar_contacto', 'password_forgot', 'password_reset'];
 
     public function __construct(
         private readonly SessionService $sessionService,
